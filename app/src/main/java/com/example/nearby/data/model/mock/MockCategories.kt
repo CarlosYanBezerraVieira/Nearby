55092d6760154df707fc3f2aade22c1e947ed4f6
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2024-12-13 17:24:07 -0300

    refactor: organizando lista de categorias em um mock

---
+++ app/src/main/java/com/example/nearby/data/model/mock/MockCategories.kt
@@ -0,0 +1,12 @@
+package com.example.nearby.data.model.mock
+
+import com.example.nearby.data.model.Category
+
+val mockCategories = listOf(
+    Category(id = "1", name = "Alimentação"),
+    Category(id = "2", name = "Compras"),
+    Category(id = "3", name = "Hospedagem"),
+    Category(id = "4", name = "Supermercado"),
+    Category(id = "5", name = "Entretimento"),
+    Category(id = "6", name = "Farmácia"),
+)
