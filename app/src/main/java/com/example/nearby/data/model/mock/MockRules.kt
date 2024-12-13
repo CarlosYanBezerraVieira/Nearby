11f0ddeaab5f6fa52324270d0576aa75ebd3f972
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2024-12-13 20:54:12 -0300

    feat: criado componentes dos detalhes do market e usando dados mocados

---
+++ app/src/main/java/com/example/nearby/data/model/mock/MockRules.kt
@@ -0,0 +1,17 @@
+package com.example.nearby.data.model.mock
+
+import com.example.nearby.data.model.Rule
+
+
+val mockRules = listOf(
+    Rule(
+        id = "012576ea-4441-4b8a-89e5-d5f32104c7c4",
+        description = "Disponível até 31/12/2024",
+        marketId = "012576ea-4441-4b8a-89e5-d5f32104c7c4"
+    ),
+    Rule(
+        id = "312576ea-4341-1b8a-03e5-d5f32204c7b1",
+        description = "Válido apenas para consumo no local",
+        marketId = "bde73364-95c5-4664-8084-79a7ca382c44"
+    )
+)
